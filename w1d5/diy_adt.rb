1fa3e0069ba8150c090f1dcc7e590e03e3ef0589
Raku Watari <rwatari@gmail.com>
2016-11-17 22:45:33 -0800

    add Map class

--- w1d5/diy_adt.rb
+++ w1d5/diy_adt.rb
@@ -33,3 +33,34 @@ class Queue
     @queue.dup
   end
 end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    idx = index(key)
+    idx ? @map[idx][1] = value : @map << [key, value]
+  end
+
+  def lookup(key)
+    idx = index(key)
+    idx ? @map[idx][1] : nil
+  end
+
+  def remove(key)
+    idx = index(key)
+    @map.delete_at(idx) if idx
+  end
+
+  def show
+    @map.map(&:dup)
+  end
+
+  private
+
+  def index(key)
+    @map.find_index { |k, _| k == key }
+  end
+end
