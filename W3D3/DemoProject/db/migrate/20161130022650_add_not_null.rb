cc69c485829642ef04fd7fdc6e7716f7ae1f8c9f
Raku Watari <rwatari@gmail.com>
2016-12-04 11:06:03 -0800

    Create gh-pages branch, add index.html

--- W3D3/DemoProject/db/migrate/20161130022650_add_not_null.rb
+++
@@ -1,7 +0,0 @@
-class AddNotNull < ActiveRecord::Migration
-  def change
-    change_column :people, :name, :string, null: false
-    change_column :people, :house_id, :integer, null: false
-    change_column :houses, :address, :string, null: false
-  end
-end
