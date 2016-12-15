922296ba161bccffde067fc3cdd604e46b36728e
Raku Watari <rwatari@gmail.com>
2016-12-14 19:09:40 -0800

    Add html to view scripts

--- W5D3/js_intro_ii.js
+++ W5D3/js_intro_ii.js
@@ -16,22 +16,26 @@ function Elephant(name, height, tricks) {
   this.name = name;
   this.height = height;
   this.tricks = tricks;
-  this.trumpet = function () {
-    console.log(`${this.name} the elephant goes 'phrRRRRRRRR!!!!!'`);
-  };
-  this.grow = function () {
-    this.height += 12;
-  };
-  this.addTrick = function (trick) {
-    this.tricks.push(trick);
-  };
-  this.play = function () {
-    let i = Math.floor(Math.random() * this.tricks.length);
-    let trick = this.tricks[i];
-    // let s = `${this.name} is ${trick}!`
-    console.log(`${this.name} is ${trick}!`);
-  };
 }
+
+Elephant.prototype.trumpet = function () {
+  console.log(`${this.name} the elephant goes 'phrRRRRRRRR!!!!!'`);
+};
+
+Elephant.prototype.grow = function () {
+  this.height += 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function () {
+  let i = Math.floor(Math.random() * this.tricks.length);
+  let trick = this.tricks[i];
+  // let s = `${this.name} is ${trick}!`
+  console.log(`${this.name} is ${trick}!`);
+};
 
 let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
 let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
@@ -40,7 +44,7 @@ let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doin
 
 let herd = [ellie, charlie, kate, micah];
 
-Elephant.paradeHelper = function(elephant) {
+Elephant.paradeHelper = function (elephant) {
   console.log(`${elephant.name} is trotting by!`);
 };
 
@@ -48,7 +52,7 @@ Elephant.paradeHelper = function(elephant) {
 
 function dinerBreakfast() {
   let order = 'cheesy scrambled eggs';
-  let say = function(additionalOrder) {
+  let say = function (additionalOrder) {
     if (additionalOrder !== undefined) {
       order += ` and ${additionalOrder}`;
     }
